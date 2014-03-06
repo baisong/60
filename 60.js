afeb62900776a8238dab04aca6a76b4b0f48d842
oren <get.oren@gmail.com>
2014-03-05 19:50:47 -0500

    more

--- 60.js
+++ 60.js
@@ -1,4 +1,37 @@
+var Sixty = Sixty || { 'delay': 1000, 'offset': 0, 'speed': 0 };
+Sixty.items = [
+  {
+    char: '0',
+    syllable: 'py',
+    decimal: '0'
+  },
+  {
+    char: '1',
+    syllable: 'pe',
+    decimal: '1'
+  },
+  {
+    char: '2',
+    syllable: 'pi',
+    decimal: '2'
+  },
+  {
+    char: '3',
+    syllable: 'pa',
+    decimal: '3'
+  },
+];
+
 (function($){
+  function goSixty() {
+    console.log('go!');
+    var item = Sixty.items[Sixty.offset];
+    $('#character').text(item.char);
+    $('#syllable').text(item.syllable);
+    $('#decimal').text(item.decimal);
+    Sixty.offset = (Sixty.offset + 1) % Sixty.items.length;
+  }
+  
   $(document).ready(function(){
     
     // Starts play mode.
@@ -8,6 +41,7 @@
     // Handles pausing and playing.
     $('.action-play').click(function(){
       console.log('Play!');
+      setInterval(goSixty, Sixty.delay);
     });
     
   });	
