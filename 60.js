61465cf508a339584d90d556185e7f1cf1bae55e
oren <get.oren@gmail.com>
2014-03-10 20:40:49 -0400

    iterate... get it?

--- 60.js
+++ 60.js
@@ -38,6 +38,40 @@ Sixty.items = [
   }
   
   /**
+	 * Converts an HSV color value to RGB. Conversion formula
+	 * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+	 * Assumes h, s, and v are contained in the set [0, 1] and
+	 * returns r, g, and b in the set [0, 255].
+	 *
+	 * @param   Number  h       The hue
+	 * @param   Number  s       The saturation
+	 * @param   Number  v       The value
+	 * @return  Array           The RGB representation
+	 *
+	 * @see https://gist.github.com/mjijackson/5311256
+	 */
+	function hsvToRgb(h, s, v) {
+		var r, g, b;
+	 
+		var i = Math.floor(h * 6);
+		var f = h * 6 - i;
+		var p = v * (1 - s);
+		var q = v * (1 - f * s);
+		var t = v * (1 - (1 - f) * s);
+	 
+		switch (i % 6) {
+		  case 0: r = v, g = t, b = p; break;
+		  case 1: r = q, g = v, b = p; break;
+		  case 2: r = p, g = v, b = t; break;
+		  case 3: r = p, g = q, b = v; break;
+		  case 4: r = t, g = p, b = v; break;
+		  case 5: r = v, g = p, b = q; break;
+		}
+	 
+		return [ r * 255, g * 255, b * 255 ];
+	}
+
+  /**
    *
    */
   function goSixtyColorGrid() {
@@ -48,7 +82,10 @@ Sixty.items = [
 		for (var i = 0; i < numRows; i++) {
 		  output += '<tr>';
 			for (var j = 0; j < numCells; j++) {
-			  var rgb = [i*50,j*4,0];
+			  var step = 360 / numColors;
+			  var offset = i*12 + j;
+			  var hue = offset * step;
+			  var rgb = hsvToRgb(hue,1,1);
 			  var color = rgb.join(',');
 				output += '<td class="sixty-color-grid" style="background:rgb(' + color+ ');">' + i + j + '</td>';
 			}
