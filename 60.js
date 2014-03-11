11712af6513a730ada0609d94784979ee33818c1
oren <get.oren@gmail.com>
2014-03-10 20:49:49 -0400

    test cyan

--- 60.js
+++ 60.js
@@ -71,6 +71,41 @@ Sixty.items = [
 		return [ r * 255, g * 255, b * 255 ];
 	}
 
+	/**
+	 * Converts an RGB color value to HSV. Conversion formula
+	 * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+	 * Assumes r, g, and b are contained in the set [0, 255] and
+	 * returns h, s, and v in the set [0, 1].
+	 *
+	 * @param   Number  r       The red color value
+	 * @param   Number  g       The green color value
+	 * @param   Number  b       The blue color value
+	 * @return  Array           The HSV representation
+	 */
+	function rgbToHsv(r, g, b) {
+		r /= 255, g /= 255, b /= 255;
+	 
+		var max = Math.max(r, g, b), min = Math.min(r, g, b);
+		var h, s, v = max;
+	 
+		var d = max - min;
+		s = max == 0 ? 0 : d / max;
+	 
+		if (max == min) {
+		  h = 0; // achromatic
+		} else {
+		  switch (max) {
+		    case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+		    case g: h = (b - r) / d + 2; break;
+		    case b: h = (r - g) / d + 4; break;
+		  }
+	 
+		  h /= 6;
+		}
+	 
+		return [ h, s, v ];
+	}
+
   /**
    *
    */
@@ -82,9 +117,9 @@ Sixty.items = [
 		for (var i = 0; i < numRows; i++) {
 		  output += '<tr>';
 			for (var j = 0; j < numCells; j++) {
-			  var step = parseFloat((1 / numColors).toFixed(4));
-			  var offset = parseFloat((i*12.0000 + j).toFixed(4));
-			  var hue = (offset * step).toFixed(4);
+			  var step = 1.0000 / numColors;
+			  var offset = i*12 + j;
+			  var hue = offset * step;
 			  var rgb = hsvToRgb(hue,1,1);
 			  var color = rgb.join(',');
 				output += '<td class="sixty-color-grid" style="background:rgb(' + color+ ');">' + i + j + '</td>';
@@ -106,7 +141,7 @@ Sixty.items = [
     $('#character').text(getCharacter(offset));
     $('#syllable').text(getSyllable(offset));
     $('#decimal').text(offset);
-    Sixty.offset = (Sixty.offset + 1) % Sixty.items.length;
+    Sixty.offset = (Sixty.offset + 1) % Sixty.items;
   }
   
   $(document).ready(function(){
@@ -115,6 +150,8 @@ Sixty.items = [
     $('#jumbotron-btn').removeClass('btn-disabled').addClass('play');
     $('.action-name').text('Play');
     $('#message-1').html(goSixtyColorGrid());
+    alert('Expected format for cyan: ' + rgbToHsv([0,255,255]));
+    
     // Handles playing.
     $('.action-play').click(function(){
       console.log('Play!');
