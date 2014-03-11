131b072622f5980b5058c53cd5348d99558a41b2
oren <get.oren@gmail.com>
2014-03-10 20:56:51 -0400

    round return

--- 60.js
+++ 60.js
@@ -68,7 +68,7 @@ Sixty.items = [
 		  case 5: r = v, g = p, b = q; break;
 		}
 	 
-		return [ r * 255, g * 255, b * 255 ];
+		return [ Math.round(r * 255), Math.round(g * 255), Math.round(b * 255) ];
 	}
 
 	/**
@@ -130,56 +130,7 @@ Sixty.items = [
 		
 		return output;
   }
-  
-  /**
-   * @see http://www.easyrgb.com/math.html
-   */
-  function hsv2rgb(h,s,v) {
-		var r, g, b;
-		var RGB = new Array();
-		if(s==0){
-			RGB['red']=RGB['green']=RGB['blue']=Math.round(v*255);
-		}else{
-			// h must be < 1
-			var var_h = h * 6;
-			if (var_h==6) var_h = 0;
-			//Or ... var_i = floor( var_h )
-			var var_i = Math.floor( var_h );
-			var var_1 = v*(1-s);
-			var var_2 = v*(1-s*(var_h-var_i));
-			var var_3 = v*(1-s*(1-(var_h-var_i)));
-			if(var_i==0){ 
-				var_r = v; 
-				var_g = var_3; 
-				var_b = var_1;
-			}else if(var_i==1){ 
-				var_r = var_2;
-				var_g = v;
-				var_b = var_1;
-			}else if(var_i==2){
-				var_r = var_1;
-				var_g = v;
-				var_b = var_3
-			}else if(var_i==3){
-				var_r = var_1;
-				var_g = var_2;
-				var_b = v;
-			}else if (var_i==4){
-				var_r = var_3;
-				var_g = var_1;
-				var_b = v;
-			}else{ 
-				var_r = v;
-				var_g = var_1;
-				var_b = var_2
-			}
-			//rgb results = 0 ÷ 255  
-			RGB['red']=Math.round(var_r * 255);
-			RGB['green']=Math.round(var_g * 255);
-			RGB['blue']=Math.round(var_b * 255);
-			}
-		return RGB;  
-		}
+
   /**
    *
    */
@@ -199,7 +150,6 @@ Sixty.items = [
     $('#jumbotron-btn').removeClass('btn-disabled').addClass('play');
     $('.action-name').text('Play');
     $('#message-1').html(goSixtyColorGrid());
-    alert('Expected RGB format for red: ' + hsv2rgb(0,1,1));
     
     // Handles playing.
     $('.action-play').click(function(){
