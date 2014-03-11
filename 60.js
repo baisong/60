c70afa22d0babdb182d6fc2b01eddba02f790ce5
oren <get.oren@gmail.com>
2014-03-10 21:02:06 -0400

    round return

--- 60.js
+++ 60.js
@@ -38,6 +38,16 @@ Sixty.items = [
   }
   
   /**
+   *
+   */
+  function getColor(offset) {
+		var step = 1.0000 / 60;
+	  var hue = offset * step;
+	  var rgb = hsvToRgb(hue,1,1);
+	  var color = 'rgb(' + rgb.join(',') + ')';
+  }
+  
+  /**
 	 * Converts an HSV color value to RGB. Conversion formula
 	 * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
 	 * Assumes h, s, and v are contained in the set [0, 1] and
@@ -69,41 +79,6 @@ Sixty.items = [
 		}
 	 
 		return [ Math.round(r * 255), Math.round(g * 255), Math.round(b * 255) ];
-	}
-
-	/**
-	 * Converts an RGB color value to HSV. Conversion formula
-	 * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
-	 * Assumes r, g, and b are contained in the set [0, 255] and
-	 * returns h, s, and v in the set [0, 1].
-	 *
-	 * @param   Number  r       The red color value
-	 * @param   Number  g       The green color value
-	 * @param   Number  b       The blue color value
-	 * @return  Array           The HSV representation
-	 */
-	function rgbToHsv(r, g, b) {
-		r /= 255, g /= 255, b /= 255;
-	 
-		var max = Math.max(r, g, b), min = Math.min(r, g, b);
-		var h, s, v = max;
-	 
-		var d = max - min;
-		s = max == 0 ? 0 : d / max;
-	 
-		if (max == min) {
-		  h = 0; // achromatic
-		} else {
-		  switch (max) {
-		    case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-		    case g: h = (b - r) / d + 2; break;
-		    case b: h = (r - g) / d + 4; break;
-		  }
-	 
-		  h /= 6;
-		}
-	 
-		return [ h, s, v ];
 	}
 
   /**
@@ -141,6 +116,7 @@ Sixty.items = [
     $('#character').text(getCharacter(offset));
     $('#syllable').text(getSyllable(offset));
     $('#decimal').text(offset);
+    $('.jumbotron').css('background-color', getColor(offset));
     Sixty.offset = (Sixty.offset + 1) % Sixty.items;
   }
   
