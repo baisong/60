8b8d55b0e0460f293806930b2c8d6e7b2e3f7870
oren <get.oren@gmail.com>
2015-04-13 09:58:45 -0700

    Update day.js

--- js/day.js
+++ js/day.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         var things = [];
         // Weekday diagram
         var today = new Date();
-        var symDate = symmetry.convert(today, 'object');
+        var symDate = symmetrical.convert(today, 'object');
         var html = '<table class="week"><tr>';
         for (var h = 0; h < symDate.weeksInMonth; h++) {
             for (var i = 0; i < 7; i++) {
