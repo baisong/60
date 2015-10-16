a6eae1ea57dd4860bde11203862bf7ccd04b4b8b
oren <get.oren@gmail.com>
2015-10-15 23:20:15 -0700

    Update day.js

--- js/day.js
+++ js/day.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         var things = [];
         // Weekday diagram
         var today = new Date();
-        today.setHours(0,0,0,0);
+        today.setHours(0, 0, 0, 0);
         var symDate = symmetrical.convert(today, 'object');
         var yearmonths = '<table class="yearmonths">';
         for (var i = 0; i < 3; i++) {
@@ -33,20 +33,19 @@ $(document).ready(function() {
                     var dayname = symDate.weekOfMonth + symDate.weekOfMonthSuffix + ' ' + symDate.dayOfWeekShort;
                     var text = '<h3>' + symDate.weekOfMonth + weekdayShort + '</h3><p>' + dayname + '</p>';
                     html += '<td class="day day-' + i + ' active">' + text + '</td>';
-                }
-                else {
+                } else {
                     html += '<td class="day day-' + i + '"><h3>&nbsp</h3><p>&nbsp</p></td>';
                 }
                 if (i == 7) {
-                	html += '</tr><tr>'
+                    html += '</tr><tr>'
                 }
             }
         }
         html += '</td></tr></table>'
         things.push(html);
         // Day of the week
-        things.push(html + '<br/>' + symDate.dayOfWeekLong);
-        things.push(html + '<br/>' + symDate.dayOfWeekLong + "<br/>" + symDate.standard);
+        things.push('reveal!');
+        things.push(html + '<br/>' + symDate.dayOfWeekLong + "," + symDate.standard);
         // DOW + Date
         var month = today.getMonth();
         var year = today.getFullYear();
@@ -75,8 +74,9 @@ $(document).ready(function() {
         // Long weekdate notation
         // Char
         var sixtyEpoch = new Date("December 27, 2014 00:00:00");
+
         function getSixtyWeekday(date) {
-            return Math.floor(parseInt(today.getTime() - sixtyEpoch.getTime()) / (1000* 60 * 60 * 24)) % 60;
+            return Math.floor(parseInt(today.getTime() - sixtyEpoch.getTime()) / (1000 * 60 * 60 * 24)) % 60;
         }
         var sixtyWeekday = getSixtyWeekday(date);
         /* test -- should be 1
@@ -108,7 +108,7 @@ $(document).ready(function() {
         things.push(rows);
         var ord = '';
         if (sixtyWeekday > 0) {
-        	ord = (sixtyWeekday + 1) + nth(sixtyWeekday + 1);
+            ord = (sixtyWeekday + 1) + nth(sixtyWeekday + 1);
         }
         things.push('toki pona: ' + Sixty.get(sixtyWeekday, 'tokipona'));
         things.push(ord + " largest country?");
@@ -123,30 +123,34 @@ $(document).ready(function() {
             var things = getThings();
             var $m = $('#message');
             var offset = parseInt($m.data('offset')) + 1;
-            if ((offset < things.length) && (things[offset].length > 0)) {
+            if (things[offset] == 'reveal!') {
+                $('.calendar td').css({color: 'white'});
+                $('.yearmonths td').css({color: 'white'});
+            }
+            else if ((offset < things.length) && (things[offset].length > 0)) {
                 $m.html(things[offset]);
                 $m.data('offset', offset);
             } else {
-            	var c = $m.data('country');
-            	var url = 'https://www.google.com/webhp#q=' + c + '&hl=en&gl=us&qscrl=1&tbm=nws&tbs=qdr:d';
+                var c = $m.data('country');
+                var url = 'https://www.google.com/webhp#q=' + c + '&hl=en&gl=us&qscrl=1&tbm=nws&tbs=qdr:d';
                 window.location.replace(url);
                 $m.text('all set!');
             }
         }
     };
     var proceed2 = function() {
-       var things = getThings();
-            var $m = $('#message');
-            var offset = parseInt($m.data('offset')) + 1;
-            if ((offset < things.length) && (things[offset].length > 0)) {
-                $m.html(things[offset]);
-                $m.data('offset', offset);
-            } else {
-            	var c = $m.data('country');
-            	var url = 'https://www.google.com/webhp#q=' + c + '&hl=en&gl=us&qscrl=1&tbm=nws&tbs=qdr:d';
-                window.location.replace(url);
-                $m.text('all set!');
-            }
+        var things = getThings();
+        var $m = $('#message');
+        var offset = parseInt($m.data('offset')) + 1;
+        if ((offset < things.length) && (things[offset].length > 0)) {
+            $m.html(things[offset]);
+            $m.data('offset', offset);
+        } else {
+            var c = $m.data('country');
+            var url = 'https://www.google.com/webhp#q=' + c + '&hl=en&gl=us&qscrl=1&tbm=nws&tbs=qdr:d';
+            window.location.replace(url);
+            $m.text('all set!');
+        }
     };
     var getTableForOffset = function(offset, char) {
         var output = '<table class="grid-60">';
@@ -169,10 +173,11 @@ $(document).ready(function() {
     };
 
     var today = new Date();
-    today.setHours(0,0,0,0);
+    today.setHours(0, 0, 0, 0);
     var sixtyEpoch = new Date("December 27, 2014 00:00:00");
+
     function getSixtyWeekday(date) {
-        return Math.floor(parseInt(date.getTime() - sixtyEpoch.getTime()) / (1000* 60 * 60 * 24)) % 60;
+        return Math.floor(parseInt(date.getTime() - sixtyEpoch.getTime()) / (1000 * 60 * 60 * 24)) % 60;
     }
     var sixtyWeekday = getSixtyWeekday(today);
     document.title = sixtyWeekday;
